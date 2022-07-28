e7796dcdc8caf11cef6c76091d638b3e52382fe5
FREDOM1979 <5baxoff@gmail.com>
2022-07-28 14:25:10 +0600

    шгез97е9

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -224,5 +224,5 @@ conn.sendButton(m.chat, `🗂️ 𝐎𝐏𝐂𝐈𝐎𝐍: ${type}
 
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
-handler.command = /^((en|dis)able|(tru|fals)e|(turn)?[01])$/i
+handler.command = /^(вкл|откл)ючить)?((en|dis)able|(tru|fals)e|(turn)?[01])$/i
 export default handler
